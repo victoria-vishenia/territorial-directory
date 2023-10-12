90ee561302a8631bf28ea45cf94b41d37a06053d
Victoria Vishenia <victoriavishnia@gmail.com>
2023-10-12 15:39:36 +0300

    P6Spy commit

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -45,8 +45,8 @@ public class Main {
         final SessionFactory sessionFactory;
         Properties properties = new Properties();
         properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/world");
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/world");
         properties.put(Environment.USER, "root");
         properties.put(Environment.PASS, "root");
         properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
